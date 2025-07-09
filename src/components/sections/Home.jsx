2964720db0705e5027591781ec12ad2b25d97e65
hamzafaisalgit <mhamzafaisal2214@gmail.com>
2025-07-10 02:11:20 +0500

    Updated

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -52,11 +52,11 @@ const Home = () => {
         </div>
 
         {/* Scroll indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+        {/* <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
           <div className="w-6 h-10 border-2 border-slate-400 rounded-full flex justify-center">
             <div className="w-1 h-3 bg-slate-400 rounded-full mt-2 animate-pulse"></div>
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
